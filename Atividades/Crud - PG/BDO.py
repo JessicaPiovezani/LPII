ad5f2bb71fe97d2df4b50891584d32441d04557a
Jessica Piovezani <jessica.piovezani@hotmail.com>
2020-10-15 21:36:24 -0300

    Adição de Validadores

--- Atividades/Crud - PG/BDO.py
+++ Atividades/Crud - PG/BDO.py
@@ -19,60 +19,72 @@ class Manipulacao():
             if (conexao):
                 cursor.close()
                 conexao.close()
-    
-    #Busca de todos os cadastros
-    def busca(self):
-        try:
-            conexao = Conexao().get_connection()
-            cursor = conexao.cursor()
-            busca = "select * from pessoas"
-            cursor.execute(busca)
-            pessoas = cursor.fetchall()
-            
-            for i in pessoas:
-                print ("Nome: ", i[1])
-                print ("CPF: ", i[0])
-                print ("E-mail: ", i[2])
-                status = i[3]
-                if status == 1:
-                    cadastro = "Ativo"
-                else:
-                    cadastro = "Inativo"
-                print ("Status: %s\n" %cadastro)
-            
-        except (Exception, psycopg2.DatabaseError) as error:
-            print("Oppss! Algum erro aconteceu :/", error)
-        
-        finally:
-            if (conexao):
-                cursor.close()
-                conexao.close()
-    
-    #Busca de cadastros Ativos
-    def abusca(self):
-        try:
-            conexao = Conexao().get_connection()
-            cursor = conexao.cursor()
-            busca = "select * from pessoas where status = 1"
-            cursor.execute(busca)
-            pessoas = cursor.fetchall()
-            
-            for i in pessoas:
-                print ("Nome: ", i[1])
-                print ("CPF: ", i[0])
-                print ("E-mail: ", i[2])
-                print ("Status: Ativo\n")
-            
-        except (Exception, psycopg2.DatabaseError) as error:
-            print("Oppss! Algum erro aconteceu :/", error)
-        
-        finally:
-            if (conexao):
-                cursor.close()
-                conexao.close()
 
-    #Busca de cadastros Inativos
-    def ibusca(self):
+    #Busca por nome de cadastros ativos
+    def busca_nome(self, nome):
+        try:
+            conexao = Conexao().get_connection()
+            cursor = conexao.cursor()
+            busca = "select * from pessoas where nome like '%{0}%' and status = 1".format(nome)
+            cursor.execute(busca)
+            pessoas = cursor.fetchall()
+            for i in pessoas:
+                print ("Nome: ", i[1])
+                print ("CPF: ", i[0])
+                print ("E-mail: ", i[2])
+            
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Oppss! Algum erro aconteceu :/", error)
+        
+        finally:
+            if (conexao):
+                cursor.close()
+                conexao.close()
+    
+    #Busca por e-mail de cadastros ativos
+    def busca_email(self, email):
+        try:
+            conexao = Conexao().get_connection()
+            cursor = conexao.cursor()
+            busca = "select * from pessoas where email like '%{0}%' and status = 1".format(email)
+            cursor.execute(busca)
+            pessoas = cursor.fetchall()
+            for i in pessoas:
+                print ("Nome: ", i[1])
+                print ("CPF: ", i[0])
+                print ("E-mail: ", i[2])
+            
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Oppss! Algum erro aconteceu :/", error)
+        
+        finally:
+            if (conexao):
+                cursor.close()
+                conexao.close()
+    
+    #Busca por CPF de cadastros ativos
+    def busca_email(self, cpf):
+        try:
+            conexao = Conexao().get_connection()
+            cursor = conexao.cursor()
+            busca = "select * from pessoas where cpf like '%{0}%' and status = 1".format(cpf)
+            cursor.execute(busca)
+            pessoas = cursor.fetchall()
+            for i in pessoas:
+                print ("Nome: ", i[1])
+                print ("CPF: ", i[0])
+                print ("E-mail: ", i[2])
+            
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Oppss! Algum erro aconteceu :/", error)
+        
+        finally:
+            if (conexao):
+                cursor.close()
+                conexao.close()
+    
+    #Busca de todos os cadastros inativos
+    def busca(self):
         try:
             conexao = Conexao().get_connection()
             cursor = conexao.cursor()
@@ -84,88 +96,6 @@ class Manipulacao():
                 print ("Nome: ", i[1])
                 print ("CPF: ", i[0])
                 print ("E-mail: ", i[2])
-                print ("Status: Inativo\n")
-            
-        except (Exception, psycopg2.DatabaseError) as error:
-            print("Oppss! Algum erro aconteceu :/", error)
-        
-        finally:
-            if (conexao):
-                cursor.close()
-                conexao.close()
-
-    #Busca por nome
-    def busca_nome(self, nome):
-        try:
-            conexao = Conexao().get_connection()
-            cursor = conexao.cursor()
-            busca = "select * from pessoas where nome like '%{0}%'".format(nome)
-            cursor.execute(busca)
-            pessoas = cursor.fetchall()
-            for i in pessoas:
-                print ("Nome: ", i[1])
-                print ("CPF: ", i[0])
-                print ("E-mail: ", i[2])
-                status = i[3]
-                if status == 1:
-                    cadastro = "Ativo"
-                else:
-                    cadastro = "Inativo"
-                print ("Status: %s\n" %cadastro)
-            
-        except (Exception, psycopg2.DatabaseError) as error:
-            print("Oppss! Algum erro aconteceu :/", error)
-        
-        finally:
-            if (conexao):
-                cursor.close()
-                conexao.close()
-    
-    #Busca por e-mail
-    def busca_email(self, email):
-        try:
-            conexao = Conexao().get_connection()
-            cursor = conexao.cursor()
-            busca = "select * from pessoas where email like '%{0}%'".format(email)
-            cursor.execute(busca)
-            pessoas = cursor.fetchall()
-            for i in pessoas:
-                print ("Nome: ", i[1])
-                print ("CPF: ", i[0])
-                print ("E-mail: ", i[2])
-                status = i[3]
-                if status == 1:
-                    cadastro = "Ativo"
-                else:
-                    cadastro = "Inativo"
-                print ("Status: %s\n" %cadastro)
-            
-        except (Exception, psycopg2.DatabaseError) as error:
-            print("Oppss! Algum erro aconteceu :/", error)
-        
-        finally:
-            if (conexao):
-                cursor.close()
-                conexao.close()
-    
-    #Busca por CPF
-    def busca_email(self, cpf):
-        try:
-            conexao = Conexao().get_connection()
-            cursor = conexao.cursor()
-            busca = "select * from pessoas where cpf like '%{0}%'".format(cpf)
-            cursor.execute(busca)
-            pessoas = cursor.fetchall()
-            for i in pessoas:
-                print ("Nome: ", i[1])
-                print ("CPF: ", i[0])
-                print ("E-mail: ", i[2])
-                status = i[3]
-                if status == 1:
-                    cadastro = "Ativo"
-                else:
-                    cadastro = "Inativo"
-                print ("Status: %s\n" %cadastro)
             
         except (Exception, psycopg2.DatabaseError) as error:
             print("Oppss! Algum erro aconteceu :/", error)
--- Atividades/Crud - PG/Cadastro.py
+++ Atividades/Crud - PG/Cadastro.py
@@ -1,21 +1,43 @@
 from BDO import Manipulacao
+from Validadores import *
 
 class Pessoa():
     def cadastro(self, cpf, nome, email):
-        self.cpf = list(input("CPF: "))
-        self.nome = list(input("Nome: "))
-        self.email = list(input("E-mail: "))
-        Tstring(cpf,nome,email)
+        self.cpf = False
+        #Looping de solicitação e validação de CPF
+        while cpf == False:
+            numero_cpf = list(input('CPF: '))
+            numero_cpf = ''.join(numero_cpf)
+            validador_cpf = ValidadorCPF(numero_cpf)
+            if validador_cpf.validador():
+                cpf = True
+            else:
+                print("CPF inválido!")
+                cpf = False
 
+        self.nome = False
+        #Looping de solicitação e validação de nome
+        while nome == False:
+            nome = list(input("Nome: "))
+            nome = ''.join(nome).upper()
+            validador_nome = ValidadorNome()
+            if validador_nome.validador(nome):
+                nome = True
+            else:
+                print("Nome inválido")
+                nome = False
 
-#Classe para converter listas em strings e inserir no banco
-class Tstring():
-    def __init__(self, cpf, nome, email):
-        self.cpf = cpf
-        self.nome = nome
-        self.email = email
-        cpf = ''.join(cpf)
-        nome = ''.join(nome).upper()
-        email = ''.join(email).upper()
+        self.email = False
+        #Looping de solicitação e validação de e-mail
+        while email == False:
+            email = list(input("E-mail: "))
+            email = ''.join(email).upper()
+            validador_email = ValidadorEmail()
+            if validador_email.validador(email):
+                email = True
+            else:
+                print("E-mail inválido")
+                email = False
+
         inserir = Manipulacao()
         return inserir.inserir(cpf,nome,email)
