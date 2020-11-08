84a32c748ea385ca5f9ef5d0c0098bd4f390b860
jessica <jessica.piovezani@alquimidia.com.br>
2020-11-08 19:36:38 -0300

    Comentários

--- N2/4/BD.py
+++ N2/4/BD.py
@@ -1,6 +1,7 @@
 import psycopg2
 
 class Conexao():
+    #Estabelecendo conexão com banco de dados
     def get_connection(self):
         conexao = psycopg2.connect(user="postgres",password="1234", host="127.0.0.1", port="5432", database="LPII")
         return conexao
