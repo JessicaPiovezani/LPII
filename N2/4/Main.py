a7a466f06648558bbf0749271cccbf5e288f57a8
jessica <jessica.piovezani@alquimidia.com.br>
2020-11-08 19:27:58 -0300

    Melhoria do Menu

--- N2/4/Main.py
+++ N2/4/Main.py
@@ -1,20 +1,29 @@
 from Pessoa import *
 from Validadores import *
 
-opcao = int(input("\n1 - Cadastrar\n2 - Editar\n3 - Remover\n4 - Consultar\nEscolha uma das opções acima: "))
+opcao = -1
 
-if opcao == 1:
-    Pessoa.cadastro()
+#Menu de interação
+while opcao!=0:
+    opcao = int(input("\n0 - Encerrar\n1 - Cadastrar\n2 - Editar\n3 - Remover\n4 - Consultar\nEscolha uma das opções acima: "))
+    
+    if opcao == 1:
+        Pessoa.cadastro()
 
-elif opcao == 2:
-    print("\n\nBusca de cadastro para edição")
-    Pessoa.busca()
-    Pessoa.edita()
+    elif opcao == 2:
+        print("\n\nBusca de cadastro para edição")
+        Pessoa.busca()
+        Pessoa.edita()
 
-elif opcao == 3:
-    print("\n\nBusca de cadastro para remoção")
-    Pessoa.busca()
-    Pessoa.inativa()
+    elif opcao == 3:
+        print("\n\nBusca de cadastro para remoção")
+        Pessoa.busca()
+        Pessoa.inativa()
 
-elif opcao == 4:
-    Pessoa.busca()
+    elif opcao == 4:
+        Pessoa.busca()
+    
+    else:
+        print("\nOpção inválida! Para encerrar o programa digite 0.")
+
+print ("Programa encerrado")
